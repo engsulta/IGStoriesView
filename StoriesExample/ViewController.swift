5542913fb4d717e05d6543b5475f406f12ca65c0
Ahmed sultan <en.ahmedsultan@gmail.com>
2020-10-06 23:41:16 +0200

    add example

--- StoriesExample/ViewController.swift
+++ StoriesExample/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var storyCardView: StoriesCardView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let allStories:[StoriesViewModel] = [
+            StoriesViewModel(image: "ahmed", name: "Your Story", isSeen: false,userId: "1"),
+            StoriesViewModel(image: "ahmed", name: "ahmed hamza",isSeen: false,userId: "2"),
+            StoriesViewModel(image: "ahmed", name: "ahmed hamza",isSeen: false,userId: "3"),
+            StoriesViewModel(image: "ahmed", name: "ahmed seen", isSeen: false,userId: "4"),
+            StoriesViewModel(image: "ahmed", name: "ahmed hamza",isSeen: false,userId: "5"),
+            StoriesViewModel(image: "ahmed", name: "ahmed hamza",isSeen: true ,userId: "6")
+        ]
+
+        storyCardView.storiesViewModels = allStories
+        storyCardView.delegate = self
     }
-
-
 }
 
+extension ViewController: StoriesCollectionViewActionsDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelect userId: String?) {
+        // present your story here
+    }
+}
