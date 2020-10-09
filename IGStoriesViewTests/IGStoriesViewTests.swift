2d7ec2ba1a9a77edd63b11b869bf53974033c791
Ahmed sultan <en.ahmedsultan@gmail.com>
2020-10-09 21:28:04 +0200

    fix UT

--- IGStoriesViewTests/IGStoriesViewTests.swift
+++ IGStoriesViewTests/IGStoriesViewTests.swift
@@ -9,19 +9,19 @@ import XCTest
 @testable import IGStoriesView
 
 class StoriesCardViewTests: XCTestCase {
-    var cardView: StoriesCardView!
+    var cardView: IGStoriesCardView!
 
     override func setUp() {
         super.setUp()
-        let allStories:[StoriesViewModel] = [
-            StoriesViewModel(image: "ahmed", name: "Your Story", isSeen: false,userId: "1"),
-            StoriesViewModel(image: "ahmed", name: "ahmed hamza",isSeen: false,userId: "2"),
-            StoriesViewModel(image: "ahmed", name: "ahmed hamza",isSeen: false,userId: "3"),
-            StoriesViewModel(image: "ahmed", name: "ahmed seen", isSeen: false,userId: "4"),
-            StoriesViewModel(image: "ahmed", name: "ahmed hamza",isSeen: false,userId: "5"),
-            StoriesViewModel(image: "ahmed", name: "ahmed hamza",isSeen: true ,userId: "6")
+        let allStories:[IGStoriesViewModel] = [
+            IGStoriesViewModel(image: "ahmed", name: "Your Story", isSeen: false,userId: "1"),
+            IGStoriesViewModel(image: "ahmed", name: "ahmed hamza",isSeen: false,userId: "2"),
+            IGStoriesViewModel(image: "ahmed", name: "ahmed hamza",isSeen: false,userId: "3"),
+            IGStoriesViewModel(image: "ahmed", name: "ahmed seen", isSeen: false,userId: "4"),
+            IGStoriesViewModel(image: "ahmed", name: "ahmed hamza",isSeen: false,userId: "5"),
+            IGStoriesViewModel(image: "ahmed", name: "ahmed hamza",isSeen: true ,userId: "6")
         ]
-        cardView = StoriesCardView(frame: .zero)
+        cardView = IGStoriesCardView(frame: .zero)
         cardView.storiesViewModels = allStories
     }
     func testCardView() {
