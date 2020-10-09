7f5396a3237fe42a9a57498269f191f582d924cc
Ahmed sultan <en.ahmedsultan@gmail.com>
2020-10-09 21:18:22 +0200

    add documentation assets

--- ExampleApp/ViewController.swift
+++ ExampleApp/ViewController.swift
@@ -12,12 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let allStories:[StoriesViewModel] = [
-            StoriesViewModel(image: "ahmed", name: "Your Story", isSeen: false,userId: "1"),
-            StoriesViewModel(image: "ahmed", name: "ahmed hamza",isSeen: false,userId: "2"),
-            StoriesViewModel(image: "ahmed", name: "ahmed hamza",isSeen: false,userId: "3"),
-            StoriesViewModel(image: "ahmed", name: "ahmed seen", isSeen: false,userId: "4"),
-            StoriesViewModel(image: "ahmed", name: "ahmed hamza",isSeen: false,userId: "5"),
-            StoriesViewModel(image: "ahmed", name: "ahmed hamza",isSeen: true ,userId: "6")
+            StoriesViewModel(image: "profile", name: "Your Story", isSeen: false,userId: "1"),
+            StoriesViewModel(image: "profile2", name: "ahmed hamza",isSeen: false,userId: "2"),
+            StoriesViewModel(image: "profile3", name: "ahmed hamza",isSeen: false,userId: "3"),
+            StoriesViewModel(image: "profile1", name: "ahmed seen", isSeen: false,userId: "4"),
+            StoriesViewModel(image: "profile1", name: "ahmed hamza",isSeen: false,userId: "5"),
+            StoriesViewModel(image: "profile1", name: "ahmed hamza",isSeen: true ,userId: "6")
         ]
 
         storyCardView.storiesViewModels = allStories
